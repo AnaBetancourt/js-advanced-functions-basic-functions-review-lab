a5452ef4f4a665d6e270ad87e29beda63efd4199
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-04-12 01:06:47 -0400

    commit tree af02dd0df550e7291a216e11b242008ed320b3b0

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+   return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(symbol = "*"){
+    return function(adjective = "special"){
+        return `You are ${symbol}${adjective}${symbol}!`
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2){return num1 + num2},
+    subtract: function(num1, num2){return num1 - num2},
+    multiply: function(num1, num2){return num1 * num2},
+    divide: function(num1, num2){return num1 / num2}
+}
+
+function actionApplyer(startingInteger, functionArray){
+    let start = startingInteger
+
+    for (let i = 0; i < functionArray.length; i++ ){
+        start = functionArray[i](start)
+    }
+
+    return start
+}
